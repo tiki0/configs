00ec2009d75dbc1756a93b49797c77914f95e300
capeta <thom.schrappe@gmail.com>
2026-09-23 17:04:38 -0400

    updated the vibe

--- nvim/lua/lazy_setup.lua
+++ nvim/lua/lazy_setup.lua
@@ -14,5 +14,33 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   { "tpope/vim-surround" },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    -- master supports nvim 0.10; the main branch needs 0.11+
+    branch = "master",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "verilog" },
+        highlight = { enable = true },
+      })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    ft = { "verilog", "systemverilog" },
+    config = function()
+      local lint = require("lint")
+      lint.linters_by_ft = {
+        verilog = { "verilator" },
+        systemverilog = { "verilator" },
+      }
+      vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
+        group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+        callback = function()
+          lint.try_lint(nil, { ignore_errors = true })
+        end,
+      })
+    end,
+  },
 })
